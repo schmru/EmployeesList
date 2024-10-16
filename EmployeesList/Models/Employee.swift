c62fe4d328c8899e26b08532d49288fdc676c131
schmru <miha.semrl@gmail.com>
2024-10-16 13:09:14 +0200

    feat: add Employee model

---
+++ EmployeesList/Models/Employee.swift
@@ -0,0 +1,14 @@
+//
+//  Employee.swift
+//  EmployeesList
+//
+//  Created by Miha on 16/10/2024.
+//
+
+import Foundation
+
+struct Employee {
+    var name: String
+    var lastName: String
+    var age: Int
+}
